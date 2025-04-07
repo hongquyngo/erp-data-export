e0bb52c559ee1fd25e581d082da66d727bbd2141
hongquyngo <quy.ngo@prostech-asia.com>
2025-04-07 15:29:51 +0700

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -5,6 +5,8 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import pytz
+import pandas as pd
+
 
 # Cấu hình logger
 logger = logging.getLogger(__name__)
